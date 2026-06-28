ffdfb12e6478dbb8b759900d7b94f1efee70905f
Mohamed solaiman <apboo5000@gmail.com>
2026-06-28 19:50:53 +0300

    dn12

--- install_flutter_and_build.sh
+++ install_flutter_and_build.sh
@@ -4,10 +4,21 @@ if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git -b stable --depth 1
 fi
 
-# 2. Fix PATH (Absolute)
-export PATH="$PWD/flutter/bin:$PATH"
+export PATH="$PATH:`pwd`/../flutter/bin"
 
-# 3. Build Command (Single line, no spaces at ends, explicit = sign)
-flutter config --enable-web
+# 3. جلب المكتبات
+echo "Fetching dependencies..."
 flutter pub get
-flutter build web --release --web-renderer=html --dart-define=SUPABASE_URL="$SUPABASE_URL" --dart-define=SUPABASE_ANON_KEY="$SUPABASE_ANON_KEY"
+
+# 4. بناء نسخة الويب
+echo "Building Web application..."
+
+flutter build web --release \
+  --dart-define=SUPABASE_URL=$SUPABASE_URL \
+  --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
+
+# 3. جلب المكتبات
+echo "Fetching dependencies..."
+flutter pub get
+
+
