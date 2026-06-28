35fa5e3395f27986ec80d460d36fafc42b6c6aea
Mohamed solaiman <apboo5000@gmail.com>
2026-06-28 14:18:56 +0300

    fix: revert to standard index.html

--- install_flutter_and_build.sh
+++ install_flutter_and_build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# 1. تحميل فلاتر بسرعة (depth 1)
+# 1. Install Flutter
 if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git -b stable --depth 1
 fi
-
-# 2. إعداد المسارات
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# 3. بناء الويب مع تمرير المتغيرات
+# 2. Build
 flutter pub get
 flutter build web --release --dart-define=SUPABASE_URL=$SUPABASE_URL --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
