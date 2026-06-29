ac234f06270a0932203f3448346e039ccc3a2f40
Mohamed solaiman <apboo5000@gmail.com>
2026-06-29 23:43:49 +0300

    dn12

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# Clone Flutter
-git clone -b stable --depth 1 https://github.com/flutter/flutter f
-export PATH="$PATH:$(pwd)/f/bin"
+# Exit on error
+set -e
+
+# Clone Flutter if it doesn't exist
+if [ ! -d "f" ]; then
+  git clone -b stable --depth 1 https://github.com/flutter/flutter f
+fi
+
+# PREPEND to PATH to ensure we use this version first
+export PATH="$(pwd)/f/bin:$PATH"
 
 # Build project
 flutter config --enable-web
 flutter pub get
-flutter build web --release --web-renderer=html \
-  --dart-define=SUPABASE_URL=$SUPABASE_URL \
-  --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
+flutter build web --release --web-renderer html --dart-define=SUPABASE_URL="$SUPABASE_URL" --dart-define=SUPABASE_ANON_KEY="$SUPABASE_ANON_KEY"
